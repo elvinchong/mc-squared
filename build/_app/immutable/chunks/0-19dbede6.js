b871cacd197cbeee10c1b1b9fb8df3eb4e64f560
Elvin Chong <cjchong12345@email.com>
2022-10-30 16:28:33 +0800

    test

---
+++ build/_app/immutable/chunks/0-19dbede6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-69d6ef8c.js";export{t as component};
---
+++ build/_app/immutable/chunks/1-b31dd2a0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ea89f250.js";export{t as component};
